9d6893839a3ab668ca896c09cd318648c08fb5d3
evtish <evtish@proton.me>
2026-01-20 00:49:50 +0300

    Create rtc_init function

--- inc/utils.h
+++ inc/utils.h
@@ -2,5 +2,7 @@
 
 #include <stdint.h>
 
+#define array_len(array) sizeof((array)) / sizeof((array)[0])
+
 int16_t limit(const int16_t val, const int16_t low, const int16_t high);
 int16_t map(const int16_t var, const int16_t low1, const int16_t high1, const int16_t low2, const int16_t high2);
--- src/rtc.c
+++ src/rtc.c
@@ -1,6 +1,36 @@
+#include "twi.c"
 #include "rtc.h"
+#include "utils.h"
 
 #define MONTH_REGISTER 0x05
+
+// TODO: init all bits in all registers with defaults values (e.g. 12/24 bit in the hours register)
+uint8_t rtc_init(void) {
+	const uint8_t register_state[] = {
+		// Clock
+		0,	// seconds BCD
+		0,	// minutes BCD
+		18 & ~(1 << 6),	// hours BCD, 12/24 bit
+		// Calendar
+		3,	// day BCD
+		49,	// date BCD
+		18, // month BCD
+		8229,	// year BCD
+		// Alarm 1
+		0 & ~(1 << 7), // seconds BCD, A1M1 bit
+		0 & ~(1 << 7), // minutes BCD, A1M2 bit
+		9 & ~(1 << 7) & ~(1 << 6), // hours BCD, A1M3 bit & 12/24 bit
+		// Alarm 2
+		(1 << 7), // A1M4 bit
+		0 & ~(1 << 7), // minutes BCD, A1M2 bit
+		33 & ~(1 << 7) & ~(1 << 6), // hours BCD, A1M3 bit & 12/24 bit
+		(1 << 7), // A1M4 bit
+		// Control
+		(1 << 2) | (1 << 1) | (1 << 0), // INTCN, A2IE, A1IE
+	};
+
+	return twi_transmit_bytes(register_state, 0x00, array_len(register_state));
+}
 
 // converts datetime from BCD to decimal (size of formatted_dt must be equal to 3 * bcd_dt_size)
 void rtc_format_datetime(char *formatted_dt, const uint8_t *bcd_dt, const uint8_t bcd_dt_size) {
